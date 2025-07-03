bc4e08c202482d3563bb72a5449511ad7165b5cb
Didem Yakici <didematilla@gmail.com>
2025-07-03 13:04:24 +0100

    version 2.0.2
    Signed-off-by: Didem Yakici <didematilla@gmail.com>

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IDlayrKit",
-            url: "https://github.com/IDlayr/idlayr-sdk-binary-trial/releases/download/2.0.1/IDlayrKit.xcframework.zip",
+            url: "https://github.com/IDlayr/idlayr-sdk-binary-trial/releases/download/2.0.2/IDlayrKit.xcframework.zip",
             checksum: "00619d721d0d5bfb68974e827d2206e0b58d114fa717fbab252e17bd4bdbca76"
             
         )
