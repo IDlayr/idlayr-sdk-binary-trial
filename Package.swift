f3496e85756fb7664d67cf0a6106dc956ad0de35
Didem Yakici <didematilla@gmail.com>
2025-07-03 12:35:38 +0100

    Update the checksum Package
    Signed-off-by: Didem Yakici <didematilla@gmail.com>

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "IDlayrKit",
             url: "https://github.com/IDlayr/idlayr-sdk-binary-trial/releases/download/2.0.1/IDlayrKit.xcframework.zip",
-            checksum: "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+            checksum: "00619d721d0d5bfb68974e827d2206e0b58d114fa717fbab252e17bd4bdbca76"
             
         )
     ]
